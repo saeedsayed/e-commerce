cf9ab6bcc6e6961f0b7eb4404ac76ba3dc1237dc
eng-saeed <eng.saeedsayed@gmail.com>
2026-02-05 14:48:17 +0200

    refactor: comment out unused imports and code in checkout page

--- app/cart/checkout/page.tsx
+++ app/cart/checkout/page.tsx
@@ -1,20 +1,20 @@
-'use client';
-import { CheckoutForm, OrderSummary } from '@/components/cartPageComponents';
-import { useCartContext } from '@/context/CartContext';
-import getStripe from '@/utils/stripe';
-import { Elements } from '@stripe/react-stripe-js';
-import { useRouter } from 'next/navigation';
+// 'use client';
+// import { CheckoutForm, OrderSummary } from '@/components/cartPageComponents';
+// import { useCartContext } from '@/context/CartContext';
+// import getStripe from '@/utils/stripe';
+// import { Elements } from '@stripe/react-stripe-js';
+// import { useRouter } from 'next/navigation';
 
 const page = () => {
-  const router = useRouter()
-  const { totalCartPrice, cartIsLoading } = useCartContext()
-  const amount = totalCartPrice.total *100
-  if(!totalCartPrice.subTotal){
-    router.back()
-  }
+  // const router = useRouter()
+  // const { totalCartPrice, cartIsLoading } = useCartContext()
+  // const amount = totalCartPrice.total *100
+  // if(!totalCartPrice.subTotal){
+  //   router.back()
+  // }
   return (
     <>
-      {
+      {/* {
         !cartIsLoading && amount>1 &&
         <Elements stripe={getStripe()} options={{ mode: 'payment', amount, currency: 'usd' }}>
           <div className='flex flex-col md:flex-row items-start gap-16'>
@@ -26,7 +26,8 @@ const page = () => {
             </div>
           </div>
         </Elements>
-      }
+      } */}
+      <p>Checkout Page</p>
     </>
   )
 }
